a63b486dd6b5f26a638775bd20a1b83a928fc5a1
Noel Tiernan <tiernolan>
2013-04-22 22:01:18 +0100

    Basic maven pom.xml

---
+++ src/main/java/org/tiernolan/nervous/network/NervousNetwork.java
@@ -0,0 +1,12 @@
+package org.tiernolan.nervous.network;
+
+/**
+ * Hello world!
+ * 
+ */
+public class NervousNetwork {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
